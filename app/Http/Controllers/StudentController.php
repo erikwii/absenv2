b28dba4518499e0109e6a169dc5e875e8382e270
dindakhrsm <dindakudo@gmail.com>
2015-12-17 13:55:32 +0700

    edit StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -11,8 +11,15 @@ use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 class StudentController extends Controller
 { public function profil(){
-
     return view('students.profil');
-
+    }
+    public function enrollmhs(){
+        return view('students/enroll');
+    }
+    public function inputabsen(){
+        return view('students/inputabsen');
+    }
+    public function lihatabsen(){
+        return view('students/lihatabsen');
     }
 }
