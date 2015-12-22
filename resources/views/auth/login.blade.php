96f685448374690d3cd5287f18902189c0dd28de
Mekas <eka.suryana@gmail.com>
2015-12-23 06:23:45 +0700

    + mockup login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,32 +7,61 @@
         </ul>
     </div>
 @endif
-<center>
-    <h1 class="page-header" style= "background-color:#222222; color:#DEDEDE; text-align:center">
-        {!! HTML::image('./img/logo.jpg', 'alt', array( 'width' => 50, 'height' => 50 )) !!} Online Presence Login
-    </h1>
+
+<link href="{!! asset('assets/css/bootstrap.css') !!}" media="all" rel="stylesheet" type="text/css"/>
+
+<h1 class="page-header" style="background-color:#222222; color:#DEDEDE; text-align:center">
+    {!! HTML::image('./img/logo.jpg', 'alt', array( 'width' => 50, 'height' => 50 )) !!} Online Presence Login
+</h1>
 
 {!! Form::open(array('url' => '/auth/login', 'class' => 'form-horizontal')) !!}
 
-    <div>
-        Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <input type="email" name="email" value="">
+<div class="form-group center-block">
+    <label class="control-label col-sm-6" for="email">
+        E-mail:
+    </label>
+    <div class="col-sm-2    ">
+        <input type="email" name="email" value="" class="form-control" placeholder="enter e-mail id">
     </div>
+</div>
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
+<div class="form-group center-block">
+    <label class="control-label col-sm-6" for="password">
+        Password:
+    </label>
+    <div class="col-sm-2    ">
+        <input type="password" name="password" value="" class="form-control" placeholder="put password">
     </div>
+</div>
 
-    <div>
+<div class="form-group">
+    <label class="control-label col-sm-6" for="empty">
+
+    </label>
+    <div class="col-sm-2">
         <input type="checkbox" name="remember"> Remember Me
     </div>
-    <br>
-    <a href="password/email">Forgot Password?</a>
-    <div>
-        <br>
-        <button type="submit">Login</button>
+</div>
+
+<div class="form-group">
+    <label class="control-label col-sm-6" for="empty">
+
+    </label>
+    <div class="col-sm-2">
+        <a href="password/email">Forgot Password?</a>
     </div>
+</div>
+
+
+
+<div class="form-group">
+    <label class="control-label col-sm-6" for="btn_login">
+
+    </label>
+    <div class="col-sm-2">
+        <button type="submit" class="btn btn-info">Login</button>
+    </div>
+</div>
+
 {!! Form::close() !!}
-    <button type="button" class="btn btn-default btn-block" onClick="window.location='/home'"><b>Back to Home</b></button>
-</center>
+<button type="button" class="btn btn-default btn-block" onClick="window.location='/home'"><b>Back to Home</b></button>
