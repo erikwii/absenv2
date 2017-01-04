978b9736cd8af3fa8f503a5013e7e5a7a16f22de
mekas <eka.suryana@gmail.com>
2017-01-04 16:48:22 +0700

    Fix enrollmhs warn page

--- resources/views/students/enroll.blade.php
+++ resources/views/students/enroll.blade.php
@@ -10,71 +10,87 @@
         </small>
     </h2>
 
-    @include('errors.error_validator')
+    <div class="container">
 
-    <h2 style="text-align:center">ENROLL MATA KULIAH</h2>
+        @include('errors.error_validator')
 
-    {!! Form::open(array('url' => '/enrollmhs', 'class' => 'form-horizontal','role'=>'form')) !!}
-    <div class="form-group form-group-sm" style="text-align: center;">
-        <div>
-            Kode Seksi
+        <h2 style="text-align:center">ENROLL MATA KULIAH</h2>
+        @if(!empty($enrollments))
+            {!! Form::open(array('url' => '/enrollmhs', 'class' => 'form-horizontal','role'=>'form')) !!}
+            <div class="form-group form-group-sm" style="text-align: center;">
+                <div>
+                    Kode Seksi
+                </div>
+            </div>
+            <div class="form-group form-group-sm">
+                <div class="col-md-5 col-sm-5"></div>
+                <div class="col-md-2 col-sm-2">
+                    <input type="text" class="form-control" name="kode_seksi" value="" title="Kode Mata Kuliah">
+                </div>
+                <div class="col-md-5 col-sm-5"></div>
+            </div>
+            <div class="form-group form-group-sm">
+                <div class="col-md-5 col-sm-5"></div>
+                <div class="col-md-1 col-sm-1">
+                    <button type="submit" class="btn btn-primary" value="Submit">Tambah</button>
+                </div>
+                <div class="col-md-1 col-sm-1">
+                    <button type="reset" class="btn btn-info" value="Reset">Reset</button>
+                </div>
+            </div>
+            <br/>
+            {!! Form::close() !!}
+        @else
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <div class="alert alert-danger">
+                        No course opened at the moment, can't enroll
+                    </div>
+                </div>
+
+                <div class="col-md-2"></div>
+            </div>
+        @endif
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>
+                                Kode Seksi
+                            </th>
+                            <th>
+                                Kode Mata Kuliah
+                            </th>
+                            <th>
+                                Nama Mata Kuliah
+                            </th>
+
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @if (!(empty($enrollments)))
+                            @foreach($enrollments as $enrollment)
+                                <tr>
+                                    <td>{!! $enrollment->seksi !!}</td>
+                                    <td>{!! $enrollment->Kode_Matkul !!}</td>
+                                    <td>{!! $enrollment->Nama_Matkul !!}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="3">No Data</td>
+                            </tr>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
         </div>
     </div>
-    <div class="form-group form-group-sm">
-        <div class="col-md-5 col-sm-5"></div>
-        <div class="col-md-2 col-sm-2">
-            <input type="text" class="form-control" name="kode_seksi" value="" title="Kode Mata Kuliah">
-        </div>
-        <div class="col-md-5 col-sm-5"></div>
-    </div>
-    <div class="form-group form-group-sm">
-        <div class="col-md-5 col-sm-5"></div>
-        <div class="col-md-1 col-sm-1">
-            <button type="submit" class="btn btn-primary" value="Submit">Tambah</button>
-        </div>
-        <div class="col-md-1 col-sm-1">
-            <button type="reset" class="btn btn-info" value="Reset">Reset</button>
-        </div>
-    </div>
-    <br/>
-    {!! Form::close() !!}
-
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>
-                        Kode Seksi
-                    </th>
-                    <th>
-                        Kode Mata Kuliah
-                    </th>
-                    <th>
-                        Nama Mata Kuliah
-                    </th>
-
-                </tr>
-                </thead>
-
-                <tbody>
-                @if (!(empty($enrollments)))
-                @foreach($enrollments as $enrollment)
-                    <tr>
-                        <td>{!! $enrollment->seksi !!}</td>
-                        <td>{!! $enrollment->Kode_Matkul !!}</td>
-                        <td>{!! $enrollment->Nama_Matkul !!}</td>
-                    </tr>
-                @endforeach
-                @else
-                    <tr>
-                        <td colspan="3">No Data</td>
-                    </tr>
-                @endif
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <div class="col-md-2"></div>
 @stop
