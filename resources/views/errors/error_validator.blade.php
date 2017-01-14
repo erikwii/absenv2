fc50f771d1edd4b69ba566523e6f14d9e6202cb8
Mekas <eka.suryana@gmail.com>
2017-01-14 16:21:59 +0700

    Fix error critical, error not shown

--- resources/views/errors/error_validator.blade.php
+++ resources/views/errors/error_validator.blade.php
@@ -1,5 +1,9 @@
-@if($errors->has(''))
-    @foreach ($errors->all() as $error)
-        <div>{{ $error }}</div>
-    @endforeach
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
